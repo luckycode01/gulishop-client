bc1c5d6640801be367a890052f3f28b3599aacc9
M1748595804 <1748595804@qq.com>
2021-07-18 14:12:55 +0800

    头部尾部组，路由组件拆分

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import Home from '@/pages/Home';
+import Search from '@/pages/Search';
+import Login from '@/pages/Login';
+import Register from '@/pages/Register';
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+  routes: [
+    {
+      path: '/home',
+      component: Home,
+    },
+    {
+      path: '/search',
+      component: Search,
+    },
+    {
+      path: '/login',
+      component: Login,
+    },
+    {
+      path: '/register',
+      component: Register,
+    },
+    {
+      path: '/',
+      redirect:'/home',
+    },
+
+  ],
+});
